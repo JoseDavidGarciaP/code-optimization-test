ca92451f1caeb85ba5fb0024f07ec8370d2ba09c
JoseDavid <jdgarciap@ufpso.edu.co>
2024-09-14 07:52:54 -0500

    Refactor complete for quiz

--- src/Store.java
+++ src/Store.java
@@ -1,16 +1,35 @@
 public class Store {
     public static void main(String[] args) {
+        //The variables p(number) are changed to arrays
+        /* 
         int p1 = 15;
         int p2 = 10;
-        int p3 = 5;
+        int p3 = 5; 
+        */
+        int [] price  = {15, 10, 5};
 
+        //An array is created for the magic values ​​of the quantity
+        int [] amount = {2, 3, 4};
+
+        //We create the comparison constant for the total salts
+        final int meter = 50;
+
+        //We create a loop to calculate the totals
+        /*   
         int total1 = p1 * 2;
         int total2 = p2 * 3;
         int total3 = p3 * 4;
-
+         */ 
+        int totalSales = 0;
+        for (int i = 0; i < price.length; i++) {
+            totalSales += (price[i]*amount[i]);
+        }
+        /* 
         int totalSales = total1 + total2 + total3;
-
-        if (totalSales > 50) {
+        */
+        //totalSales = 80;
+        
+        if (totalSales > meter) {
             System.out.println("Good sales performance");
         } else {
             System.out.println("Low sales performance");
--- src/TaxCalculation.java
+++ src/TaxCalculation.java
@@ -1,13 +1,23 @@
 public class TaxCalculation {
     public static void main(String[] args) {
+        //We create the arrangements for prices and percentages.
+        /* 
         double productPrice1 = 100;
         double productPrice2 = 200;
         double tax1 = productPrice1 * 0.15;
-        double tax2 = productPrice2 * 0.10;
-        double totalTax = tax1 + tax2;
+        double tax2 = productPrice2 * 0.10; 
+        */
+
+        double [] productPrice = {100, 200};
+        double [] discountPercentages = {0.15, 0.10};
+
+        //We create the comparison constant for the total discountPercentages
+        final int meter = 50;
         
-        if (totalTax > 50) {
-            System.out.println("High total tax: " + totalTax);
+        double totalDiscount = tax1 + tax2;
+        
+        if (totalDiscount > meter) {
+            System.out.println("High total tax: " + totalDiscount);
         } else {
             System.out.println("Low tax");
         }
